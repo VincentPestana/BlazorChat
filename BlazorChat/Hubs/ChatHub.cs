19ff21e058d4adafa9f19a8b5650be7c52e8baa6
Vincent Pestana <crashoveride001@gmail.com>
2020-05-01 19:53:54 +0200

    Removed RoomID. Added hub

---
+++ BlazorChat/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using BlazorChat.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat.Hubs
+{
+	public class ChatHub : Hub
+	{
+		private Dictionary<string, ChatRoom> _rooms;
+		public async Task<string> CreateRoom(string roomName, string createdByUser)
+		{
+			// Check if name exists
+			if (RoomExists(roomName))
+				return "Room name exists";
+
+			_rooms.Add(roomName, new ChatRoom { CreatedByUser = createdByUser });
+
+			return "";
+		}
+
+		public async Task<string> JoinRoom(string roomName, string userName)
+		{
+
+		}
+
+		private bool RoomExists(string roomName)
+		{
+			return _rooms.ContainsKey(roomName);
+		}
+	}
+}
---
+++ BlazorChat/Models/ChatRoom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat.Models
+{
+	public class ChatRoom
+	{
+		public string CreatedByUser { get; set; }
+	}
+}
