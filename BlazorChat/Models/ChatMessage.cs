3112184d5c8f095cd277aa052eeec025bc4761ab
Vincent Pestana <crashoveride001@gmail.com>
2020-05-04 21:05:30 +0200

    Added new model for chat messages, also implementation

---
+++ BlazorChat/Models/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat.Models
+{
+	public class ChatMessage
+	{
+		public string Username { get; set; }
+		public DateTime DateSent { get; set; }
+		public string Text { get; set; }
+	}
+}
